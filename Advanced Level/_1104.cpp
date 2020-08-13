e7e5c1f851fd1eac41a6ec8b73ca681198f1d4da
bsdl <bsdl@qq.com>
2020-08-14 01:22:02 +0800

    😵

--- Advanced Level/_1104.cpp
+++ Advanced Level/_1104.cpp
@@ -3,27 +3,16 @@
 
 using namespace std;
 
-vector<float> v;
-float sum = 0;
-
+int n;
+long double total = 0;
+long double data[100002] = {0};
 
 int main() {
-	int n;
-	cin >> n;
-	v.resize(n);
-	for (int i = 0; i < n; i++) {
-		scanf("%f", &v[i]);
-	}
-	//vector<float> sub;
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
-			float tmp = 0;
-			for (int k = i; k <= j; k++) {
-				tmp += v[k];
-			}
-			sum += tmp;
-		}
-	}
-	printf("%.2f", sum);
-	return 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+        total += data[i] * (i + 1) * (n - i);
+    }
+    printf("%.2Lf", total);
+    return 0;
 }
