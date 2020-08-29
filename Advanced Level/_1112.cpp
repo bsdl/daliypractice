743e8dd8258723d36ab04252e8033fed2910a631
bsdl <bsdl@qq.com>
2020-08-30 03:21:03 +0800

    😪

--- Advanced Level/_1112.cpp
+++ Advanced Level/_1112.cpp
@@ -1,54 +1,65 @@
 #include <iostream>
 #include <set>
+#include <map>
 
 using namespace std;
 
 int main() {
-	int k;
-	cin >> k;
-	string str;
-	cin >> str;
-	string stucked = "";
-	set<char> s;
-	set<char> occured;
-	int len = str.length();
-	int left = 0, right = 0;
-	int cnt = 0;
-	while (right < len) {
-		if (str[left] == str[right]) {
-			cnt++;
-			if (cnt == k) {
-				if (occured.find(str[left]) != occured.end()) {
-					right++;				
-					stucked.append(str.substr(left, right - left));
-					left = right;
-				}
-				else {
-					s.insert(str[left]);
-					stucked += str[left];
-					right++;
-					left = right;				
-				}	
-				cnt = 0;
-				continue;
-			}
-			right++;
-			if (right == len) {
-				stucked.append(str.substr(left, right - left));
-				break;
-			}
-		}
-		else {
-			stucked.append(str.substr(left, right - left));
-			occured.insert(str[left]);
-			left = right;
-			cnt = 0;
-		}
-	}
-	for (char ch : s) {
-		printf("%c", ch);
-	}
-	printf("\n");
-	cout << stucked;
+    int k;
+    scanf("%d\n", &k);
+    string str;
+    getline(cin, str);
+    map<char, bool> notStucked;
+    set<char> mightStucked;
+    int left = 0, right = 1;
+    while (right < str.length()) {
+        if (str[left] != str[right]) {
+            notStucked[str[left]] = true;
+            left++;
+            right++;
+        } else {
+            while (right < str.length() && str[left] == str[right]) right++;
+            int len = right - left;
+            if (len < k) {
+                notStucked[str[left]] = true;
+                left = right - 1;
+            } else if (len > k) {
+                if (len % k == 0) {
+                    mightStucked.insert(str[left]);
+                    left = right;
+                    right++;
+                } else {
+                    notStucked[str[left]] = true;
+                }
+            } else {
+                mightStucked.insert(str[left]);
+                left = right;
+                right++;
+            }
+
+        }
+    }
+    set<char> stucked;
+    for (auto &it : mightStucked) {
+        if (!notStucked[it]) {
+            stucked.insert(it);
+        }
+    }
+    string resStr;
+    map<char, bool> appeared;
+    int idx = 0;
+    while (idx < str.length()) {
+        resStr += str[idx];
+        if (stucked.count(str[idx]) == 0) {
+            idx++;
+        } else {
+            if (!appeared[str[idx]]) {
+                printf("%c", str[idx]);
+                appeared[str[idx]] = true;
+            }
+            idx += k;
+        }
+    }
+    printf("\n%s", resStr.c_str());
 	return 0;
 }
