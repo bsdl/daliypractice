b361f72420f751f4fd6fbc20dd54adf605620831
bsdl <bsdl@qq.com>
2020-08-30 15:06:51 +0800

    😄

--- Advanced Level/_1032.cpp
+++ Advanced Level/_1032.cpp
@@ -1,56 +1,38 @@
-//
-// Created by bsdl on 2020/6/17.
-//
-
 #include <iostream>
 #include <set>
 
 using namespace std;
 
-//struct node {
-//    int addr;
-//    bool isVisited;
-//    int next;
-//};
-//
-//int main() {
-//    node linkedList[100000];
-//    int addr1, addr2, n;
-//    scanf("%d %d %d", &addr1, &addr2, &n);
-//    for (int i = 0; i < n; i++) {
-//        int addr, letter, next;
-//        scanf("%d %c %d", &addr, &letter, &next);
-//        linkedList[addr] = {addr, false, next};
-//    }
-//    for (int a = addr1; a != -1; a = linkedList[a].next) {
-//        linkedList[a].isVisited = true;
-//    }
-//    int start = addr2;
-//    while (!linkedList[start].isVisited) {
-//        start = linkedList[start].next;
-//    }
-//    if (start == -1) {
-//        cout << -1;
-//    } else {
-//        printf("%05d", linkedList[start].addr);
-//    }
-//    return 0;
-//}
+struct Node {
+    char data;
+    int next;
+}linkedList[100000];
 
 int main() {
-    set<int> s;
-    int addr1, addr2, n;
-    scanf("%d %d %d", &addr1, &addr2, &n);
+    int head1, head2, n;
+    scanf("%d %d %d", &head1, &head2, &n);
+    set<int> addrs;
     for (int i = 0; i < n; i++) {
-        int addr, letter, next;
-        scanf("%d %c %d", &addr, &letter, &next);
-        if (next != -1 && s.find(next) == s.end()) {
-            s.insert(next);
-        } else if (next != -1) {
-            printf("%05d", next);
+        int addr, next;
+        char data;
+        scanf("%d %c %d", &addr, &data, &next);
+        linkedList[addr].data = data;
+        linkedList[addr].next = next;
+    }
+    int node = head1;
+    while (node != -1) {
+        addrs.insert(node);
+        node = linkedList[node].next;
+    }
+    node = head2;
+    while (node != -1) {
+        if (addrs.count(node) > 0) {
+            printf("%05d", node);
             return 0;
+        } else {
+            node = linkedList[node].next;
         }
     }
-    cout << -1;
+    printf("-1");
     return 0;
 }
