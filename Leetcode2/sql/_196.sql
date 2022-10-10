f9d50d87116ef7ef2cc1ea45c61724dfee6803ed
bsdl <bsdl@qq.com>
2022-10-10 23:19:15 +0800

    o(><；)oo

---
+++ Leetcode2/sql/_196.sql
@@ -0,0 +1,13 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+delete from Person 
+where id not in (
+    select id 
+    from (
+        select min(id) 
+        as id
+        from Person 
+        group by 
+        email
+    ) n
+);
